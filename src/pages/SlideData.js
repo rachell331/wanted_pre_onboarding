e73d48b0a47df9e6dbfb3f79e524bcf9797a6b8a
standup8 <borapark13@gmail.com>
2022-01-19 15:47:41 +0900

    Modify: go to Next/Prev page using click button

---
+++ src/pages/SlideData.js
@@ -0,0 +1,44 @@
+export const SLIDE_DATA = [
+  {
+    id: 1,
+    title: '개발자 성공 비결 공개',
+    subtitle: 'Velog, 글 잘 쓰는 개발자들의 이야기',
+    imageUrl: 'images/main1.jpg',
+  },
+  {
+    id: 2,
+    title: '개발자 되고 싶은 분들?',
+    subtitle: '프론트엔드 무료 교육과정 참여하기',
+    imageUrl: 'images/main2.jpg',
+  },
+  {
+    id: 3,
+    title: '포트폴리오를 부탁해!',
+    subtitle: '디자이너의 포폴 살펴보기',
+    imageUrl: 'images/main3.jpg',
+  },
+  {
+    id: 4,
+    title: '성과를 내는 마케팅',
+    subtitle: '실제 사례를 공개합니다!',
+    imageUrl: 'images/main4.jpg',
+  },
+  {
+    id: 5,
+    title: '스타트업 레퍼런스',
+    subtitle: '지금 바로 참여해보세요!',
+    imageUrl: 'images/main5.jpg',
+  },
+  {
+    id: 6,
+    title: '해, 커리어 EP 02 공개',
+    subtitle: '마지막 관문 2라운드의 승자는?',
+    imageUrl: 'images/main6.jpg',
+  },
+  {
+    id: 7,
+    title: 'HR마스터 과정 선공개',
+    subtitle: 'HR의 마스터가 될 수 있는 기회',
+    imageUrl: 'images/main7.jpg',
+  },
+];
